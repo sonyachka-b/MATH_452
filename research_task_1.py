29fcb743d215da4ab129ec41e6ca70618aae2dfb
sonyachka-b <skb6445@psu.edu>
2025-08-03 22:30:45 +0200

    added a plot, a graph and a hash map

--- research_task_1.py
+++ research_task_1.py
@@ -1,5 +1,10 @@
 import numpy as np 
+import matplotlib.pyplot as mplt
 #make a big for loop for N 
+
+eigenvector_orthogonality = dict()
+
+# mapping: matrix -> [dot_1_2, dot_4_8, ...]
 
 for N in range (50, 1000, 50):
 # Creating S with r = 10 where S is a square diagonal matrix
@@ -10,7 +15,11 @@ for N in range (50, 1000, 50):
  #           [8.2 7 5 6.5 7 5.3 8 6.7 7.1 5][6.3 9 8.1 7 9 6.4 8.2 7.9 9.1 5.6][5 6.3 7 9.2 8.8 7.3 9 8 5.8 6.3]
   #          [9 8.9 7.1 6 5 6 5.5 6.7 5.2 7][5.7 9 8.7 7.3 6.2 9 5.5 7.9 5 6.1][5.7 9.2 8.8 7.4 8 9.1 5.6 7.2 6.9 9.2]
    #         [5.2 9 8 6 7 9.3 8.9 5.1 7 6.1]]
-   matrix_S = np.zeros(N, N)
+
+   # initialize mapping for N
+   eigenvector_orthogonality[N] = []
+
+   matrix_S = np.zeros((N, N))
    matrix_S[1][1] = 10
    matrix_S[2][2] = 9.5
    matrix_S[3][3] = 8.7
@@ -45,7 +54,7 @@ for N in range (50, 1000, 50):
 
    for i in range (N):
       for j in range (N):
-         matrix_R[N][N] = (1/N) * np.random.standard_normal(1)
+         matrix_R[i][j] = (1 / N) * np.random.standard_normal(1)
 
    R_final = matrix_R + matrix_R.T - np.diag(matrix_R.diagonal()) #look for an easier way
    R_final = np.round(R_final, 2)
@@ -70,18 +79,18 @@ for N in range (50, 1000, 50):
 # Dot products of same indicies
    dot_1_1 = np.dot(U_w[:, 1], U_s[:, 1])
    squared_dot_1_1 = np.abs(dot_1_1)**2
-   print (squared_dot_1_1)
+   eigenvector_orthogonality[N].append(("squared_dot_1_1", squared_dot_1_1))
 
    dot_2_2 = np.dot(U_w[:, 2], U_s[:, 2])
    squared_dot_2_2 = np.abs(dot_2_2)**2
-   print (squared_dot_2_2)
+   eigenvector_orthogonality[N].append(("squared_dot_2_2", squared_dot_2_2))
 
    dot_3_3 = np.dot(U_w[:, 3], U_s[:, 3])
    squared_dot_3_3 = np.abs(dot_3_3)**2
-   print (squared_dot_3_3)
+   eigenvector_orthogonality[N].append(("squared_dot_3_3", squared_dot_3_3))
 
 # Dot products of different indicies
-
+'''
    dot_1_2 = np.dot(U_w[:, 1], U_s[:, 2])
    squared_dot_1_2 = np.abs(dot_1_2)**2
    print (squared_dot_1_2)
@@ -93,7 +102,7 @@ for N in range (50, 1000, 50):
    dot_5_8 = np.dot(U_w[:, 5], U_s[:, 8])
    squared_dot_5_8 = np.abs(dot_5_8)**2
    print (squared_dot_5_8)
-
+'''
 
 
 # start with cmparing same indicies <u_tilda_1, u_1> and fixed r: |<u_tilda_1, u_1>|^2 ---> 1 - 1/(corresp. sing. value)^2
@@ -120,6 +129,23 @@ for N in range (50, 1000, 50):
 
 # Take a dor product <ui, ui_tilda>
 
+'''
+Matplotlib Graphs
+'''
+mplt.xlabel("N")
+mplt.ylabel("|<u_tilda_1, u_1>|^2")
+x_axis = np.arange(50, 1000, 50)
+
+y_axis = []
+for k in eigenvector_orthogonality:
+   y_axis.append(eigenvector_orthogonality[k][0][1])
+
+# [1,2,3], [3,6,9]
+
+mplt.plot(x_axis, y_axis, marker="o", linestyle="-", color="blue", label="y = 2x")
+
 # Big N loop 
 
-
+#print("Orthogonality Value Mappings:")
+#print (eigenvector_orthogonality)
+mplt.show()
