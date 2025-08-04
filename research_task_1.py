a9dec14e80818aa5db3759444ce2be95c1c60d4e
sonyachka-b <skb6445@psu.edu>
2025-08-04 21:30:53 +0200

    added graphs for same and different indicies

--- research_task_1.py
+++ research_task_1.py
@@ -47,18 +47,18 @@ for N in range (50, 1000, 50):
 # Assign the values by hand (something like 5, 6, 7 etc.)
 #matrix_S = np.round(matrix_S, 2) #rounding 
 
-   print(matrix_S)
+   #print(matrix_S)
 
 # Creating R with rnadom entries with N(0, 1/N)
    matrix_R = np.zeros((N,N))
 
    for i in range (N):
       for j in range (N):
-         matrix_R[i][j] = (1 / N) * np.random.standard_normal(1)
+         matrix_R[i][j] = (1 / N) * np.random.standard_normal(1)[0]
 
    R_final = matrix_R + matrix_R.T - np.diag(matrix_R.diagonal()) #look for an easier way
    R_final = np.round(R_final, 2)
-   print(R_final)
+   #print(R_final)
 
 # Finidng W = R+S
 
@@ -66,15 +66,15 @@ for N in range (50, 1000, 50):
 
 # SVD for W (output will be Uw, Sum and Vw)
    U_w, Sigma_w, Vt_w = np.linalg.svd(W, True, True, False)
-   print (U_w)
-   print (Sigma_w)
-   print (Vt_w)
+   #print (U_w)
+   #print (Sigma_w)
+   #print (Vt_w)
 
 #SVD for S just to make it faster 
    U_s, Sigma_s, Vt_s = np.linalg.svd(matrix_S, True, True, False)
-   print (U_s)
-   print (Sigma_s)
-   print (Vt_s)
+   #print (U_s)
+   #print (Sigma_s)
+   #print (Vt_s)
 # Uw = Uw' => to compare W' and S we take the <u_tilda_i, u_i> where u_tilda_i is in Uw=Uw' and u_i is in Vs which is a identity matrix bc S is diagonal
 # Dot products of same indicies
    dot_1_1 = np.dot(U_w[:, 1], U_s[:, 1])
@@ -90,19 +90,20 @@ for N in range (50, 1000, 50):
    eigenvector_orthogonality[N].append(("squared_dot_3_3", squared_dot_3_3))
 
 # Dot products of different indicies
-'''
+
    dot_1_2 = np.dot(U_w[:, 1], U_s[:, 2])
    squared_dot_1_2 = np.abs(dot_1_2)**2
-   print (squared_dot_1_2)
+   eigenvector_orthogonality[N].append(("squared_dot_1_2", squared_dot_1_2))
+
 
    dot_3_4 = np.dot(U_w[:, 3], U_s[:, 4])
    squared_dot_3_4 = np.abs(dot_3_4)**2
-   print (squared_dot_3_4)
+   eigenvector_orthogonality[N].append(("squared_dot_3_4", squared_dot_3_4))
 
    dot_5_8 = np.dot(U_w[:, 5], U_s[:, 8])
    squared_dot_5_8 = np.abs(dot_5_8)**2
-   print (squared_dot_5_8)
-'''
+   eigenvector_orthogonality[N].append(("squared_dot_5_8", squared_dot_5_8))
+
 
 
 # start with cmparing same indicies <u_tilda_1, u_1> and fixed r: |<u_tilda_1, u_1>|^2 ---> 1 - 1/(corresp. sing. value)^2
@@ -129,23 +130,92 @@ for N in range (50, 1000, 50):
 
 # Take a dor product <ui, ui_tilda>
 
+print("Orthogonality Value Mappings:")
+print (eigenvector_orthogonality)
 '''
 Matplotlib Graphs
 '''
+# Figure 1 " Same indicies vs N"
+# Plot 1 "|<u_tilda_1, u_1>|^2 vs N"
+mplt.figure(1)
+mplt.title("Same indicies d.p. vs N")
 mplt.xlabel("N")
-mplt.ylabel("|<u_tilda_1, u_1>|^2")
-x_axis = np.arange(50, 1000, 50)
+mplt.ylabel("|<u_tilda_i, u_i>|^2")
 
+x_axis = np.arange(50, 1000, 50)
 y_axis = []
 for k in eigenvector_orthogonality:
    y_axis.append(eigenvector_orthogonality[k][0][1])
+mplt.plot(x_axis, y_axis, marker="o", linestyle="-", color="blue", label="y = 2x")
+ref_curve_1 = 0.95
+mplt.plot(ref_curve_1, linestyle='-', color='black', label='1 - 1/100 (ref)')
+mplt.legend()
 
-# [1,2,3], [3,6,9]
+# Plot 2 "|<u_tilda_2, u_2>|^2 vs N"
+#mplt.title("dot_2_2 vs N")
+#mplt.xlabel("N")
+#mplt.ylabel("|<u_tilda_2, u_2>|^2")
+mplt.figure(2)
+x_axis = np.arange(50, 1000, 50)
+y_axis = []
+for k in eigenvector_orthogonality:
+   y_axis.append(eigenvector_orthogonality[k][1][1])
+mplt.plot(x_axis, y_axis, marker="o", linestyle="-", color="green", label="y = 2x")
 
+# Plot 3 "|<u_tilda_3, u_3>|^2 vs N"
+#mplt.xlabel("N")
+#mplt.ylabel("|<u_tilda_2, u_2>|^2")
+mplt.figure(3)
+x_axis = np.arange(50, 1000, 50)
+y_axis = []
+for k in eigenvector_orthogonality:
+   y_axis.append(eigenvector_orthogonality[k][2][1])
+mplt.plot(x_axis, y_axis, marker="o", linestyle="-", color="red", label="y = 2x")
+
+
+
+# Figure 2 " Different indicies vs N"
+# Plot 1 "|<u_tilda_1, u_2>|^2 vs N"
+mplt.figure(4)
+mplt.title("Different indicies d.p. vs N")
+mplt.xlabel("N")
+mplt.ylabel("|<u_tilda_i, u_j>|^2")
+
+x_axis = np.arange(50, 1000, 50)
+y_axis = []
+for k in eigenvector_orthogonality:
+   y_axis.append(eigenvector_orthogonality[k][3][1])
 mplt.plot(x_axis, y_axis, marker="o", linestyle="-", color="blue", label="y = 2x")
 
-# Big N loop 
+# Plot 2 "|<u_tilda_2, u_2>|^2 vs N"
+#mplt.title("dot_2_2 vs N")
+#mplt.xlabel("N")
+#mplt.ylabel("|<u_tilda_2, u_2>|^2")
 
-#print("Orthogonality Value Mappings:")
-#print (eigenvector_orthogonality)
+x_axis = np.arange(50, 1000, 50)
+y_axis = []
+for k in eigenvector_orthogonality:
+   y_axis.append(eigenvector_orthogonality[k][4][1])
+mplt.plot(x_axis, y_axis, marker="o", linestyle="-", color="green", label="y = 2x")
+
+# Plot 3 "|<u_tilda_3, u_3>|^2 vs N"
+#mplt.xlabel("N")
+#mplt.ylabel("|<u_tilda_2, u_2>|^2")
+x_axis = np.arange(50, 1000, 50)
+y_axis = []
+for k in eigenvector_orthogonality:
+   y_axis.append(eigenvector_orthogonality[k][5][1])
+mplt.plot(x_axis, y_axis, marker="o", linestyle="-", color="red", label="y = 2x")
+
+# Plot 4 - to compare 
+ref_curve = 1 / (x_axis ** 2)
+
+mplt.plot(x_axis, ref_curve, linestyle='-', color='black', label='1/N² (ref)')
+mplt.legend()
+
+print("Orthogonality Value Mappings:")
+print (eigenvector_orthogonality)
+
 mplt.show()
+
+
